608d243faa4ee887840a7b6a14e906f639969610
saidelatar <@gmail.com>
2024-12-27 23:24:49 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/warsha60/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.warsha60
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Warsha60());
+}
+
+class Warsha60 extends StatelessWidget {
+  const Warsha60({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(),
+    );
+  }
+}
